b159441db43d7b90dc6cd06fb7c35f8e74fe1cee
Lucas Lombardo <lucasalombardo@gmail.com>
2023-09-25 00:46:28 -0400

    [BAR] Adds baseline structure and test toggle button

---
+++ projects/bar-chart/components/App.js
@@ -0,0 +1,11 @@
+import ToggleTests from "./ToggleTests";
+
+const App = () => (
+  <main>
+    <h1>US GDP Bar Chart</h1>
+    <section className="content">content</section>
+    <ToggleTests testId="bar-chart" />
+  </main>
+);
+
+export default App;
---
+++ projects/bar-chart/components/ToggleTests.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { sleep } from "../../../shared/utils";
+
+const ToggleTests = ({ testId }) => {
+  const [displayed, setDisplayed] = useState(null);
+
+  const onClick = async () => {
+    const previousValue = displayed;
+    setDisplayed(!previousValue);
+    if (previousValue === null) {
+      try {
+        await sleep(100);
+        const root = document.querySelector(
+          "#fcc_test_suite_wrapper",
+        ).shadowRoot;
+        // expand fcc test menu if not open
+        if (!root.querySelector(".fcc_hamburger.transform_top")) {
+          root.querySelector("#fcc_toggle").click();
+        }
+        await sleep(100);
+        // select correct test suite
+        root.querySelector("#test-suite-selector").value = testId;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <button className="toggle-tests" onClick={onClick}>
+        Toggle FCC Tests
+      </button>
+      <style>
+        {`#fcc_test_suite_wrapper {
+            display: ${displayed ? "block" : "none"}
+        }`}
+      </style>
+    </>
+  );
+};
+
+export default ToggleTests;
